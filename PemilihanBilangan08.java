cf2bf5afe1ebb8419db7cbfe78de251d21838e49
MeutiaArafah <meutiaarafah@gmail.com>
2024-09-27 16:16:43 +0700

    Percobaan 1 Ternary Op

--- PemilihanBilangan08.java
+++ PemilihanBilangan08.java
@@ -6,15 +6,21 @@ import java.util.Scanner;
 public class PemilihanBilangan08 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
 
-        System.out.print("Masukkan sebuah angka: ");
-        int angka = sc.nextInt();
+        String hasil;
+        int angka = 4;
+        // Scanner sc = new Scanner (System.in);
 
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        // System.out.print("Masukkan sebuah angka: ");
+        // int angka = sc.nextInt();
+
+        // if (angka % 2 == 0) {
+        //     System.out.println("Angka " + angka + " termasuk bilangan genap");
+        // } else {
+        //     System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        // }
+
+        hasil = (angka % 2 == 0) ? "Bilangan genap" : "Bilangan ganjil";
+        System.out.println(angka + " termasuk " + hasil);
     }
 }
