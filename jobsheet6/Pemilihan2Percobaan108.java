d975292fbad157c8a3e057c9b6c19e2e32647b38
MeutiaArafah <meutiaarafah@gmail.com>
2024-10-05 22:14:13 +0700

    ini modifikasi 1

--- jobsheet6/Pemilihan2Percobaan108.java
+++ jobsheet6/Pemilihan2Percobaan108.java
@@ -13,11 +13,13 @@ public class Pemilihan2Percobaan108 {
         System.out.print("Masukkan Tahun : ");
         tahun = input08.nextInt();
 
-        if ((tahun % 4 ) == 0) {
-            if ((tahun % 100) != 0) 
+        if ((tahun % 400 ) == 0) {
+            if ((tahun % 100) != 0) // pengecualian 1, tidak habis dibagi 100
                 System.out.println("Tahun Kabisat");
+        
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
+        
     }
 }
