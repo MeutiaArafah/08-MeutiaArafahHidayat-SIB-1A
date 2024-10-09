eb85e2a3adc620b98dad4d59f9068e0bdae9f05c
MeutiaArafah <meutiaarafah@gmail.com>
2024-10-10 05:28:22 +0700

    modifikasi ke 1  equals

--- jobsheet6/Pemilihan2Percobaan308.java
+++ jobsheet6/Pemilihan2Percobaan308.java
@@ -30,7 +30,7 @@ public class Pemilihan2Percobaan308 {
             }
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.println("Penghasilan Bersih : " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+        } else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000) {
                 pajak = 0.15;
             } else if (penghasilan <= 3500000) {
