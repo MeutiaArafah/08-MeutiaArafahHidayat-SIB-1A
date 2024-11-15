1f8faeeab4d235f0d48589a49bf31b8524170ab6
MeutiaArafah <meutiaarafah@gmail.com>
2024-11-15 21:14:40 +0700

    modifikasi nomor 2

--- jobsheet9/ArrayRataNilai08.java
+++ jobsheet9/ArrayRataNilai08.java
@@ -2,30 +2,38 @@ import java.util.Scanner;
 
 public class ArrayRataNilai08 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMhs = sc.nextInt();
+
+        int[] nilaiMhs = new int[jumlahMhs];
         double total = 0;
-        double rata2;
-        int mhsLulus = 0;
+        double rataLulus, rataTdkLulus, totalLulus = 0, totalTdkLulus = 0;
+        int mhsLulus = 0, mhsTdkLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
 
             if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
                 mhsLulus++;
-                
-            }
-        }
 
+            } else {
+                totalTdkLulus += nilaiMhs[i];
+                mhsTdkLulus++;
+            }
+
+        }
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
         }
-        
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
-        System.out.println("Jumlah mahasiswa yang lulus: " + mhsLulus);        
+
+        rataLulus = totalLulus / mhsLulus;
+        rataTdkLulus = totalTdkLulus / mhsTdkLulus;
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTdkLulus);
     }
 
 }
--- jobsheet9/SearchNilai08.java
+++ jobsheet9/SearchNilai08.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class SearchNilai08 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+
+        Scanner sc = new Scanner(System.in);
+
+        // int[] arrNilai;
         int hasil = 0;
 
-        for(int i = 0; i < arrNilai.length; i++){
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int nilai = sc.nextInt();
+        int arrNilai[] = new int[nilai];
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin di cari: ");
+        int key = sc.nextInt();
+        for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
                 hasil = i;
                 break;
@@ -13,5 +27,6 @@ public class SearchNilai08 {
         System.out.println();
         System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
         System.out.println();
+
     }
 }
