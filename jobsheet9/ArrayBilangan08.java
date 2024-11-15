3b27dac04c4d3e65eb876933dc808f5abf10f24d
MeutiaArafah <meutiaarafah@gmail.com>
2024-11-15 15:18:49 +0700

    modifikasi nomor 2

--- jobsheet9/ArrayBilangan08.java
+++ jobsheet9/ArrayBilangan08.java
@@ -1,12 +1,12 @@
 public class ArrayBilangan08 {
 
     public static void main(String[] args) {
-        int[] bil = new int[4];
+        int[] bil = {5, 13, -7, 17};
 
-        bil[0] = 5.0;
-        bil[1] = 12867;
-        bil[2] = 7.5;
-        bil[3] = 2000000;
+        // bil[0] = 5.0;
+        // bil[1] = 12867;
+        // bil[2] = 7.5;
+        // bil[3] = 2000000;
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);
