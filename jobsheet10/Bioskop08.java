8ad6601ba28fe61c2305e4c664a0834408641b6a
MeutiaArafah <meutiaarafah@gmail.com>
2024-11-23 08:47:09 +0700

    modifikasi nomor 9

--- jobsheet10/Bioskop08.java
+++ jobsheet10/Bioskop08.java
@@ -22,15 +22,16 @@ public class Bioskop08 {
     // System.out.println(penonton[2].length);
     // System.out.println(penonton[3].length);
 
-    System.out.println("Penonton pada baris ke-3: ");
+    // System.out.println("Penonton pada baris ke-3: ");
 
-    for(String i : penonton[2]){
-        System.out.println(i);
+    for(int i = 0; i < penonton.length; i++){
+        System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
     }
 
-    // for(int i = 0; i < penonton[2].length; i++){
-    //     System.out.println(penonton[2][i]);
+    // for(String i : penonton[2]){
+    //     System.out.println(i);
     // }
+
 
     
 
