7c5dbdf8d36420d32a9e2acda97d8b27c7bfda08
MeutiaArafah <meutiaarafah@gmail.com>
2024-11-30 16:34:23 +0700

    percobaan 2

--- Pertemuan13/Kafe08.java
+++ Pertemuan13/Kafe08.java
@@ -1,9 +1,13 @@
 public class Kafe08 {
     // kalo ada void gabisa langsung di sout, harus dipanggil di void main
-    public static void Menu() {
-    }
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
 
-    public static void main(String[] args) {
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -13,5 +17,9 @@ public class Kafe08 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+
+    public static void main(String[] args) {
+        Menu("Andi", true);
     }
 }
