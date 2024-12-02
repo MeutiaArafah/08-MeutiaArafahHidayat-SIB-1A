ceaa3bfda09772cfd0a09169bd235a39e35a0e25
MeutiaArafah <meutiaarafah@gmail.com>
2024-12-02 16:05:05 +0700

    percobaan 4 modifikasi nomor 2

--- Pertemuan13/PengunjungCafe08.java
+++ Pertemuan13/PengunjungCafe08.java
@@ -2,18 +2,14 @@ public class PengunjungCafe08 {
 
     public static void daftarPengunjung(String... namaPengunjung){
         System.out.println("Daftar Nama Pengunjung:");
-        for(int i = 0; i < namaPengunjung.length; i++){
-            System.out.println("- " + namaPengunjung[i]);
-
+        for (String p : namaPengunjung) {
+            System.out.println("- " + p);
+                    
         }
     }
+
     public static void main(String[] args) {
-        daftarPengunjung("Ali", "Budi", "Citra");
+       daftarPengunjung("Ali", "Budi", "Citra");
     }
 }
 
-// belum di push, benerin percobaan 3 dulu
-//for (String p : namaPengunjung) {
-//     System.out.println("- " + namaPengunjung);
-            
-// }
